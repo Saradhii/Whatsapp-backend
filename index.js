53f93ca44b7e255238850da2e11a9ba6ba52d4e7
Saradhi <99667252+Saradhii@users.noreply.github.com>
2022-08-30 23:44:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(
     cors({
-      origin: ["http://localhost:3000","https://harmonious-clafoutis-0cfde6.netlify.app"],
+      origin: ["http://localhost:3000","https://whatsapp-frontend-rouge.vercel.app"],
     })
   );
 app.use("/user",UserRoute);
